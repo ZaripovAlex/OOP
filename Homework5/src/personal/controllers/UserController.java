6ee3f528a97a4c0b609a8b062bd765df1c903e8b
Зарипов Алексей <112966921+ZaripovAlex@users.noreply.github.com>
2023-02-25 21:41:34 +0500

    Add files via upload

---
+++ Homework5/src/personal/controllers/UserController.java
@@ -0,0 +1,69 @@
+package personal.controllers;
+
+import personal.model.Repository;
+import personal.model.User;
+import personal.views.Validation;
+
+import java.util.List;
+
+public class UserController {
+    private final Repository repository;
+    private final Validation validator;
+
+    public UserController(Repository repository, Validation validator)
+    {
+        this.validator = validator;
+        this.repository = repository;
+    }
+
+    public void saveUser(User user) throws Exception {
+        validator.validateUser(user);
+        repository.CreateUser(user);
+    }
+
+    public User readUser(String userId) throws Exception {
+        List<User> users = repository.getAllUsers();
+        User user = userSearch(userId, users);
+        return user;
+    }
+
+    private static User userSearch(String userId, List<User> users) throws Exception{
+        for (User user : users) {
+            if (user.getId().equals(userId)) {
+                return user;
+            }
+        }
+        throw new Exception("User not found");
+    }
+
+    public List<User> readAllUsers(){
+        return repository.getAllUsers();
+    }
+
+    public void updateUser(String userId, User newUser) throws Exception {
+        validator.validateUser(newUser);
+        List<User> users = repository.getAllUsers();
+        User user = userSearch(userId,users);
+        user.setFirstName(newUser.getFirstName());
+        user.setLastName(newUser.getLastName());
+        user.setPhone(newUser.getPhone());
+        repository.saveUsers(users);
+    }
+
+    public void deleteUser(String userId) throws Exception {
+        List<User> users = repository.getAllUsers();
+        User user = userSearch(userId,users);
+        users.remove(user);
+        reindex(users);
+        repository.saveUsers(users);
+        System.out.println("Done");
+
+    }
+
+    private void reindex(List<User> users) {
+        int index = 0;
+        for (User item: users) {
+            item.setId(""+ (index+1));
+            index++;
+        }
+}}
