67cea06574c2ba2f20f6fbdf52dd9a6ea18e48b4
Зарипов Алексей <112966921+ZaripovAlex@users.noreply.github.com>
2023-03-05 20:04:41 +0500

    Delete Homework7/Notes directory

--- Homework7/Notes/src/main/Homework Notes/model/AbstractNotes.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public abstract class AbstractNotes {
-    String Id;
-    String text;
-    abstract public String getId();
-    abstract public void setId(String id);
-    abstract public String ToString();
-}
